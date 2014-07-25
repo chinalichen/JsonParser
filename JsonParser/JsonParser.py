39390dc60a98112de65991cbc6bd0706523e3a44
chinalichen <chinalichen@126.com>
2014-07-25 15:28:15 +0800

    add function
    parse_string()
    parse_value()
    parse_object() and so on.

--- JsonParser/JsonParser.py
+++ JsonParser/JsonParser.py
@@ -1,1 +1,56 @@
-print('Hello World')
+str = '{"a":12,"b":3,"cdasfewafdsafefdsaf":"dsafe"}'
+index = 0
+length = len(str)
+
+def parse_json():
+    parse_object()
+    print "success"
+
+def parse_object():
+    global index, length, str
+    while index < length:
+        char = str[index+1]
+        if char == '}':
+            index+=1
+            print "object end, pos:" , index
+            return True
+        elif char == '"':
+            parse_attribute()
+        elif ' ':
+            index+=1
+        elif ',':
+            index+=1
+        else:
+            raise SyntaxError(index)
+
+def parse_attribute():
+    global index, length, str
+    index+=1
+    parse_string()
+    if not str[index] == ':':
+        raise SyntaxError(index)
+    index+=1
+    parse_value()
+
+def parse_value():
+    global index, length, str
+    index+=1
+    while True:
+        if str[index] == ',':
+            index-=1
+            break;
+        if str[index] == '}':
+            index-=1
+            break;
+        index+=1
+
+def parse_string():
+    global index, length, str
+    index+=1;
+    while True:
+        if str[index] == '"':
+            index+=1
+            break;
+        index+=1
+
+parse_json()
